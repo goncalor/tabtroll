c9aa7df9ae4f6843a0c8aed26dfbcc4cf19bce04
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2019-03-29 13:15:18 +0000

    Only close tabs if more than x are open

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
+const allowedTabs = 5
+
 chrome.tabs.onCreated.addListener(function() {
   chrome.tabs.query({}, function(tabs) {
+    if(tabs.length <= allowedTabs) {
+      return;
+    }
     id = tabs[tabs.length - 1].id;
     chrome.tabs.remove(id, function() {});
   });
